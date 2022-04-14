c9dced142438bb6a57118fa609f7e8db21d203d3
Mudit Tiwari <mudittiwari2000@gmail.com>
2022-04-14 19:49:46 +0530

    WIP - Contact Page (#2)

---
+++ src/assets/svg/UserActiveIcon.tsx
@@ -0,0 +1,32 @@
+import { SvgIcon, SvgIconProps } from '@mui/material'
+
+const UserActiveIcon = (props: SvgIconProps) => {
+  return (
+    <SvgIcon
+      id="Component_13_1"
+      data-name="Component 13 – 1"
+      xmlns="http://www.w3.org/2000/svg"
+      width="23.054"
+      height="27.215"
+      viewBox="0 0 23.054 27.215"
+      {...props}
+    >
+      <path
+        id="Path_25544"
+        data-name="Path 25544"
+        d="M0,92.429c0-3.945,5.275-4.967,11.528-4.967,6.218,0,11.526.986,11.526,4.931s-5.272,4.965-11.526,4.965C5.31,97.359,0,96.373,0,92.429"
+        transform="translate(0 -70.143)"
+        fill="#1daf9e"
+      />
+      <path
+        id="Path_25545"
+        data-name="Path 25545"
+        d="M17.9,7.629a7.629,7.629,0,1,1,7.629,7.629A7.629,7.629,0,0,1,17.9,7.629"
+        transform="translate(-13.999)"
+        fill="#035d62"
+      />
+    </SvgIcon>
+  )
+}
+
+export default UserActiveIcon
